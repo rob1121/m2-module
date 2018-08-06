9a394ac1aff09b9e7ffeda80c4890ea7083bb71f
Robinson L. Legaspi <robinson.legaspi@yahoo.com>
2018-08-07 00:50:18 +0800

    add custom attribute to customer

--- HelloWorld/Controller/Index/Test.php
+++ HelloWorld/Controller/Index/Test.php
@@ -15,7 +15,9 @@ class Test extends \Magento\Framework\App\Action\Action
 
 	public function execute()
 	{
-		echo "Hello World";
+		$textDisplay = new \Magento\Framework\DataObject(array('text' => 'Mageplaza'));
+		$this->_eventManager->dispatch('mageplaza_helloworld_display_text', ['mp_text' => $textDisplay]);
+		echo $textDisplay->getText();
 		exit;
 	}
 }
