c0f3d711df4b67846eb4face893357097b358db6
Robinson Legaspi <rob@manila1902>
2018-08-08 17:44:54 +0800

    mage 2 tutorial

---
+++ HelloWorld/Block/Banner.php
@@ -0,0 +1,9 @@
+<?php namespace Mageplaza\HelloWorld\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+
+class Banner extends Template
+{
+    
+}
