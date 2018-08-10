afcaae75ed5cb93f8df94e434447f2bc5974a224
Robinson Legaspi <rob@manila1902>
2018-08-10 17:49:12 +0800

    theming tuts

--- DropdownMenu/registration.php
+++
@@ -1,3 +0,0 @@
-<?php use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Mageplaza_DropdownMenu', __DIR__);
--- TopContact/registration.php
+++
@@ -1,3 +0,0 @@
-<?php use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Mageplaza_TopContact', __DIR__);
